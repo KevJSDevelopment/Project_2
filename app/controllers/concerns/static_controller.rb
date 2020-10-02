66502817dea77dde5ab5317c64b6485f76e26696
theprivatepark <anneypark@gmail.com>
2020-10-02 11:51:44 -0400

    finished

--- app/controllers/concerns/static_controller.rb
+++ app/controllers/concerns/static_controller.rb
@@ -1,6 +1,11 @@
 class StaticController < ApplicationController
 
   def greetings
+    @bestdeals = Item.all.min_by(10) {|item| item.price}
+    @items_rec = []
+    (10).times do 
+      @items_rec << Item.all.sample
+    end
     render :greetings
   end
 
